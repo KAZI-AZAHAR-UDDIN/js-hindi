3e6bf0469f9b2061ed2e804f7433ae45a5974245
KAZI-AZAHAR-UDDIN <86212566+KAZI-AZAHAR-UDDIN@users.noreply.github.com>
2023-03-13 07:23:41 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let scrore =null
+// console.log(typeof scrore);
+// console.log(typeof(scrore));
+
+// let valueInNumber =Number(scrore);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn ="kazi" //true =0, when its  =0 means false
+
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log( typeof stringNumber);
