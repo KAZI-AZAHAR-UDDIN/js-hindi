9f9f14a7fb1c0c8609e4b983d1341c475429713f
KAZI-AZAHAR-UDDIN <86212566+KAZI-AZAHAR-UDDIN@users.noreply.github.com>
2023-03-13 07:49:13 +0000

    conversion datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,6 +11,16 @@
 
 // let booleanIsloggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsloggedIn);
-let someNumber =33
-let stringNumber =String(someNumber)
-console.log( typeof stringNumber);
+// let someNumber =33
+// let stringNumber =String(someNumber)
+// console.log( typeof stringNumber);
+// let value =3
+// let negValue =+value
+// console.log(negValue);
+
+let str1 = "hellow"
+let str2 ="kazi"
+let str3 = str1+str2  //link to study : https://tc39.es/ecma262/#sec-type-conversion
+console.log(str3);
+console.log("1"+2);
+console.log(1+2+"3");
